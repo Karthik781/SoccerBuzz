db9d1ffa0b89a99d3d8118bd7eb0fe7d5d048ed3
Karthik Kedam <karthikmsdian78@gmail.com>
2021-04-28 23:52:36 +0530

    set-up players list component

--- src/components/player-list.component.js
+++ src/components/player-list.component.js
@@ -1,6 +1,24 @@
 import React , { Component} from "react"
+import axios from "../../node_modules/axios";
 
 export default class PlayerList extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            players: [],
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/players')
+        .then(res=> {
+            this.setState({players : res.data})
+        }).catch(err=> console.log(err))
+
+        console.log(this.state);
+    }
 
     render(){
         return(
